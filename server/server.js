2ee254981c7a41e4acff814e295074b6a85a03d2
Luca Williams <ljwilliams@colgate.edu>
2025-07-07 20:20:26 -0400

    working on server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,11 @@
 const express = require('express');
-const path = require('path')
 const app = express();
 
+app.listen(3000)
+
+app.get('/', (res, req) =>{
+    console.log('Here')
+    res.send('/Users/lucawilliams/Desktop/Meal-Prep-App/public/login.html')
+})
+
+app.listen(3000)
