3fcbe6cd8ede60e10fed7f3ff1570487224021ad
Luca Williams <ljwilliams@colgate.edu>
2025-07-08 21:53:28 -0400

    controller file added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-const path = require('path')
+const path = require('path');
 
-const port = 3000
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public'))); 
 
