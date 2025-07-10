32a6f95094ef159d911853b8584e0de7183ee6dd
Luca Williams <ljwilliams@colgate.edu>
2025-07-09 22:36:33 -0400

    beginning register frontend

---
+++ public/js/register.js
@@ -0,0 +1,11 @@
+const first = document.getElementById("first")
+const last = document.getElementById("last")
+const email = document.getElementById("email")
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+
+const form = document.getElementById("registerForm")
+
+form.addEventListener('registerForm', (event) => {
+    console.log("Clicked!")
+})
