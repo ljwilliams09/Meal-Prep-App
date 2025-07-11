27a2937b179c7214e9ed8b87233d9ca156e2fb4f
Luca Williams <ljwilliams@colgate.edu>
2025-07-10 20:45:33 -0400

    login handler

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,16 @@
-const userId = document.getElementById('username')
-const password = document.getElementById('password')
+document.getElementById('loginUser').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
 
-const form = document.getElementById('loginUser')
+    const response =  await fetch('/auth', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    if (!response.ok || !result.success){
+        alert(result.error || "Login Failed" )
+    };
+});
