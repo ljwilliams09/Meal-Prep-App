342429f7ccb87416ebfe742e32347ab29e447283
Luca Williams <ljwilliams@colgate.edu>
2025-07-11 14:10:15 -0400

    implementing argon

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const argon2 = require('argon2');
 
 const public = path.join(__dirname, '..', '..', 'public')
 
@@ -18,7 +19,7 @@ router.post('/authLogin', async (req, res) =>{
     const { username, password } = req.body;
     // validate here
 
-    // lookup in database
+    // Lookup user
     try {
         const user = await prisma.user.findFirst({
             where: {
@@ -27,11 +28,20 @@ router.post('/authLogin', async (req, res) =>{
                     { email : username }
                 ]
             }
-        })
+        });
+
+        if (!user){
+            return res.status(401).json({ error: 'Invalid creditials.' });
+        }
     }
     catch (err){
-
+        return res.status(501).json({error: 'Something went wrong on our end.' });
     }
+    // Validate password
+    const verified = await argon.verify(user.password, password)
+    if (!verified)
+        return res.status()
+
 });
 
 module.exports = router;
